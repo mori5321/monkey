cccf905475b8d2b8b71e2eba3c3f263ea4e2e475
mori5321 <moriwm77@gmail.com>
2023-06-25 02:08:34 +0900

    feat: impl evaluator for integer & boolean

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"example.com/monkey/evaluator"
 	"example.com/monkey/lexer"
 	"example.com/monkey/parser"
 )
@@ -32,8 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
